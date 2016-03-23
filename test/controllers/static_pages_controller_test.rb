84a5d9b70044f3ee76e77c19c947c1a78f6153ff
Nyron-Waite <nyroncurtiswaite@gmail.com>
2016-03-23 11:37:33 -0400

    blogsetup

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -17,7 +17,6 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get about" do
     get :about
     assert_response :success
-      assert_select "h1", "More info?"
       assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
