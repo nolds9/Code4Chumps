57ca126d48f5bc78fb598bb538ac0b156f28a20b
Nyron-Waite <nyroncurtiswaite@gmail.com>
2016-03-24 08:44:11 -0400

    setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/home'
+  get 'home' => 'static_pages#home'
 
-  get 'static_pages/help'
+  get 'help' => 'static_pages#help'
 
-  get 'static_pages/about'
+  get 'about' => 'static_pages#about'
+
+  get 'signup'  => 'users#new'
 
   root 'static_pages#home'
+
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,5 +19,4 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
       assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
-
 end
